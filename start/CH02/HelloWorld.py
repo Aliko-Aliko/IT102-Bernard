edd2dfe3abf18dbd7db4ea25c5449e4545a03d20
Aliko.Aliko <Mwambungu.Bernard.greenriver.edu>
2025-10-12 12:31:33 -0700

    https://github.com/Aliko-Aliko/IT102-Bernard.git

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
-# A simple "Hello World" script in python
-# Created 
-print("test")
+# A simple 'Hello world' script in python
+# created by Bernard
+
+# Ask the user for their name
+name =input('what is your names?')
+# print('Hello ' + name'!')
+
+# print a positive message
+print('Today is going to be a great day!')
+
+# Ask for age and  calculate future age
+age = int(input('How old are you?'))
+future_age = age + 2
+
+print(f' In 2 years, you will be {future_age} years old')b
